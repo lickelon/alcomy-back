3bb5668ea021b58bfdc39835e639580c4be6ef04
lickelon <jindoman0@naver.com>
2023-05-31 17:19:55 +0900

    /recipe/list/ api added

--- main.py
+++ main.py
@@ -20,5 +20,17 @@ app.add_middleware(
 
 @app.get("/recipe/{recipe_id}")
 async def RecipeById(recipe_id : int):
-    
     return {"name" : recipe[recipe_id]}
+
+@app.get("/recipe/list/")
+async def RecipeList(sort : str | None = None, pivot : str | None = None):
+    if sort == 'asc':
+        return {"id_list" : list(range(1,17))}
+    elif sort == 'desc':
+        return {"id_list" : list(range(1,17)[::-1])}
+    else:
+        return {"id_list" : list(range(1,17))}
+
+@app.get("/recipeStep/{recipe_id}/{step_num}")
+async def RecipeStepById(recipe_id : int, step_num : int):
+    return {"step_info" : "this is dummy step info"}
